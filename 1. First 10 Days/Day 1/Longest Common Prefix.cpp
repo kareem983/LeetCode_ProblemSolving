a200dacedec2e63ba44a5e689e825bae8915a634
kareem saeed <karimsaid026@gmail.com>
2021-10-18 05:34:44 +0200

    Day 1 - Problem 3

---
+++ 1. First 10 Days/Day 1/Longest Common Prefix.cpp
@@ -0,0 +1,28 @@
+/*
+Problem Title: Longest Common Prefix
+Problem Link: https://leetcode.com/problems/longest-common-prefix/
+*/
+
+
+#include <vector>
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+     string common = ""; bool isDone = true;
+
+	for (int i = 0; i < strs[0].size(); i++) {
+		char co = strs[0].at(i);
+		
+		for (int j = 0; j < strs.size(); j++) {
+			if (i >= strs[j].size() || co != strs[j].at(i)) {
+				isDone = false; break;
+			}
+		}
+		if (!isDone) break;
+		common.push_back(co);
+	}
+        
+        return common;
+        
+    }
+};
