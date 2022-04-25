6e993e8984907281ab23547607da49e980b85b40
kareem saeed <karimsaid026@gmail.com>
2022-04-26 01:02:25 +0200

    Update Day1 with problem 1

--- 1.First_10_Days/Day_1/Two_Sum.cpp
+++ 1.First_10_Days/Day_1/Two_Sum.cpp
@@ -6,18 +6,38 @@ Problem Link: https://leetcode.com/problems/two-sum/
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans; bool isOk = false;
-        for(int i=0;i<nums.size();i++){
-            for(int j= i+1 ;j<nums.size();j++){
-                if( nums[i] + nums[j] == target){
-                    ans.push_back(i); ans.push_back(j);
-                    isOk = true; break;
-                }
+int val1 = 0, val2 = 0;
+    
+    vector<int> getIndexes(vector<int>& nums) {
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size() ; i++){
+            if(val1 != INT_MAX && val1 == nums[i]){
+                 ans.push_back(i);
+                 val1 = INT_MAX;
             }
-            if(isOk)break;
+            else if(val2 != INT_MAX && val2 == nums[i]){
+                 ans.push_back(i);
+                 val2 = INT_MAX;
+            }
         }
+        
         return ans;
+    
+    }
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int ptr1 = 0, ptr2 = nums.size()-1;
+        vector<int>copy = nums;
+        
+        sort(copy.begin(), copy.end());
+        while(copy[ptr1] + copy[ptr2] != target){
+            if(copy[ptr1] + copy[ptr2] > target) ptr2--;
+            else ptr1++;
+        }
+        val1 = copy[ptr1];
+        val2 = copy[ptr2];   
+        
+        return getIndexes(nums);
     }
 };
-
