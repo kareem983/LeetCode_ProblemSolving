c47a7c489eef77ee126ba710c45717089c50175f
kareem saeed <karimsaid026@gmail.com>
2022-05-16 23:47:58 +0200

    Update problem 1 in day 1

--- 1.First_10_Days/Day_1/Palindrom_Number.cpp
+++ 1.First_10_Days/Day_1/Palindrom_Number.cpp
@@ -4,18 +4,15 @@ Problem Link: https://leetcode.com/problems/palindrome-number/
 */
 
 
+
 class Solution {
 public:
-   bool isPalindrome(int x) {
-    bool isP = true;
-	string num = to_string(x);
-	for (int i = 0; i < num.size() / 2; i++) {
-		if (num[i] != num[num.size() - i - 1]) {
-			isP = false; break;
-		}
-	}
-     if (isP)return true;
-	 return false;
-}
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        string copy = num;
+        reverse(copy.begin(), copy.end());
+        if(num == copy)return true;
+        
+        return false;
+    }
 };
- 
