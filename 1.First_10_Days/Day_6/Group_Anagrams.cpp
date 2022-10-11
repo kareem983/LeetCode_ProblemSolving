49ac6340c5623bb4ff314e1562bd31ddc01c53af
kareem saeed <karimsaid026@gmail.com>
2022-10-11 23:31:22 +0200

    Adding another solution to problem 1 from day 6

--- 1.First_10_Days/Day_6/Group_Anagrams.cpp
+++ 1.First_10_Days/Day_6/Group_Anagrams.cpp
@@ -29,3 +29,39 @@ public:
         return ans;
     }
 };
+
+
+
+/*Another Solution With AsciCode and Hash Table*/
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> umap;
+        
+        for(int i=0;i<strs.size();i++){
+            string key = "";
+            int arr[26] ={};
+            
+            for(int j=0;j<strs[i].size();j++)
+                arr[strs[i].at(j) - 'a']++;
+            
+            for (int temp = 0; temp < 26; temp++)
+                    key += to_string(arr[temp]) + ",";
+            
+            umap[key].push_back(strs[i]);
+        }
+        
+        for(auto it: umap){
+            vector<string> tmp_ans;
+            for(int i=0 ; i<it.second.size();i++)
+                tmp_ans.push_back(it.second[i]);
+            
+            ans.push_back(tmp_ans);
+        }
+        
+        return ans;
+    }
+};
