86ce27104c223d460b5385540805f140567f0082
kareem saeed <karimsaid026@gmail.com>
2022-12-22 11:39:14 +0200

    Adding Another Solution to problem 2 from day 11

--- 2.Second_10_Days/Day_11/Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ 2.Second_10_Days/Day_11/Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -4,6 +4,7 @@ Problem Link: https://leetcode.com/problems/find-first-and-last-position-of-elem
 */
 
 
+/************************* Using STLs O(Log(N)) ***************************/
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
@@ -16,3 +17,52 @@ public:
         return {lower, upper};
     }
 };
+
+
+
+
+/************************ Using Implementation  O(Log(N)) ****************/
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lower = getlower(0, nums.size()-1, target, nums);
+        int upper = getupper(0, nums.size()-1, target, nums);
+        
+        if(lower == -1 || upper == -1) return {-1,-1};
+        return {lower, upper};
+    }
+    
+    
+    
+    int getlower(int start, int end, int target, vector<int>& nums){
+        int mid = (start + end)/2;
+        if(start > end) return -1;
+        else if(nums[mid] == target){
+            if(mid >0 && nums[mid-1] == target)
+                return getlower(start, mid-1, target, nums);
+            else return mid;
+        }
+        else if(nums[mid] < target)
+            return getlower(mid+1, end, target, nums);
+        else 
+            return getlower(start, mid-1, target, nums);
+    }
+
+    
+    int getupper(int start, int end, int target, vector<int>& nums){
+        int mid = (start + end)/2;
+        if(start > end) return -1;
+        else if(nums[mid] == target){
+            if(mid <nums.size()-1 && nums[mid+1] == target)
+                return getupper(mid+1, end, target, nums);
+            else return mid;
+        }
+        else if(nums[mid] < target)
+           return getupper(mid+1, end, target, nums);
+        else 
+           return getupper(start, mid-1, target, nums);
+    }
+};
+
